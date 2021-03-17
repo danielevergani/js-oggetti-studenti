fbf8ef989c528fa20b2d04f049c66469d2bc35a1
danielevergani <dvergani94@gmail.com>
2021-03-17 15:30:41 +0100

    inserire oggetti con ciclo while

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
-var studente = {
-    nome: "Daniele",
-    cognome: "Vergani",
-    eta: 27,
-};
+// var studente = {
+//     nome: "Daniele",
+//     cognome: "Vergani",
+//     eta: 27,
+// };
 
-console.log(studente);
+// console.log(studente);
 
-for (var key in studente){
-    console.log(key + " " + studente[key]);
-}
+// for (var key in studente){
+//     console.log(key + " " + studente[key]);
+// }
 
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
@@ -34,22 +34,31 @@ var studenti = [
     },
 ];
 
-for( var i = 0; i < studenti.length; i++ ){
-    console.log( studenti[i]["nome"] + " " + studenti[i]["cognome"] );
-}
+// for( var i = 0; i < studenti.length; i++ ){
+//     console.log( studenti[i]["nome"] + " " + studenti[i]["cognome"] );
+// }
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nome;
+var cognome;
+var eta;
+var aggiunta = "si";
 
-var nome = prompt("inserisci un nome");
-var cognome = prompt("inserici un cognome");
-var eta = prompt("quanti anni ha lo studente?");
+while( aggiunta == "si" ){
 
-var nuovoStudente = {};
+    var nuovoStudente = {};
 
-nuovoStudente["nome"] = nome;
-nuovoStudente["cognome"] = cognome;
-nuovoStudente["eta"] = eta;
-// console.log(nuovoStudente);
+    nome = prompt("inserisci un nome");
+    cognome = prompt("inserici un cognome");
+    eta = parseInt(prompt("quanti anni ha lo studente?"));
 
-studenti.push(nuovoStudente);
+    nuovoStudente["nome"] = nome;
+    nuovoStudente["cognome"] = cognome;
+    nuovoStudente["eta"] = eta;
+
+    studenti.push(nuovoStudente);
+
+    aggiunta = prompt("devi aggiundere altri studenti?")
+}
+
 console.log(studenti);
