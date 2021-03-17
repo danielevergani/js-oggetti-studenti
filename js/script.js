792425080f1569fbdb80e60a5a8ced9d43a01aed
danielevergani <dvergani94@gmail.com>
2021-03-17 15:05:55 +0100

    array di aggetti + ciclo for e stampo nome cognome

--- js/script.js
+++ js/script.js
@@ -2,17 +2,40 @@
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
 var studente = {
-    nume: "Daniele",
+    nome: "Daniele",
     cognome: "Vergani",
-    eta: 27
+    eta: 27,
 };
+
+console.log(studente);
 
 for (var key in studente){
     console.log(key + " " + studente[key]);
 }
 
-
-
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+var studenti = [
+    {
+        nome: "Daniele",
+        cognome: "Vergani",
+        eta: 27,   
+    },
+    {
+        nome: "Samuele",
+        cognome: "Madrigali",
+        eta: 29,   
+    },
+    {
+        nome: "Michele",
+        cognome: "Stucchi",
+        eta: 23,   
+    },
+];
+
+for( var i = 0; i < studenti.length; i++ ){
+    console.log( studenti[i]["nome"] + " " + studenti[i]["cognome"] );
+}
+
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
