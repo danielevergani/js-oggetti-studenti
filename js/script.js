c5d8687b4fa0bcf32d8f300f262e4077ac6c041a
danielevergani <dvergani94@gmail.com>
2021-03-17 15:44:52 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
-// var studente = {
-//     nome: "Daniele",
-//     cognome: "Vergani",
-//     eta: 27,
-// };
+var studente = {
+    nome: "Daniele",
+    cognome: "Vergani",
+    eta: 27,
+};
 
-// console.log(studente);
+console.log(studente);
 
-// for (var key in studente){
-//     console.log(key + " " + studente[key]);
-// }
+for (var key in studente){
+    console.log(key + " " + studente[key]);
+}
 
 // Creare un array di oggetti di studenti.
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
@@ -34,9 +34,9 @@ var studenti = [
     },
 ];
 
-// for( var i = 0; i < studenti.length; i++ ){
-//     console.log( studenti[i]["nome"] + " " + studenti[i]["cognome"] );
-// }
+for( var i = 0; i < studenti.length; i++ ){
+    console.log( studenti[i]["nome"] + " " + studenti[i]["cognome"] );
+}
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 var nome;
